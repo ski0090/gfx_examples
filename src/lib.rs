dad78ade50a4f44c9d99a354d9c496142cb5752f
Shalom <ski0090@naver.com>
2022-01-21 12:07:30 +0900

    Init project

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,5 @@ use proc_macro::TokenStream;
 
 #[proc_macro]
 pub fn make_answer(_: TokenStream) -> TokenStream {
-    "fn answer(a:u32) -> u32 {a}"
-    .parse().unwrap()
+    "fn answer(a:u32) -> u32 {a}".parse().unwrap()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,31 @@
-extern crate hello_macro;
-use hello_macro::make_answer;
+extern crate gfx_backend_vulkan as back;
+use std::ffi::c_void;
 
-make_answer!();
+use gfx_hal::{adapter::Adapter, Instance};
+
 fn main() {
-    println!("{}", answer(3));
+    let instance = back::Instance::create("test", 1).expect("Failed to create an instance!");
+
+    let gpu = selected_gpu(&instance);
+
+    let direct_display = match std::env::var("DIRECT_DISPLAY") {
+        Ok(_) => true,
+        Err(_) => false,
+    };
+
+    let hwnd: *mut c_void = c_void::__variant1;
+
+    let surface = unsafe {
+        instance
+            .create_surface(&hwnd)
+            .expect("Failed to create a surface!")
+    };
 }
 
+fn selected_gpu<I: gfx_hal::Instance<B>, B: gfx_hal::Backend>(instance: &I) -> Adapter<B> {
+    let mut adapters = instance.enumerate_adapters();
+    for adapter in &adapters {
+        println!("{:?}", adapter.info);
+    }
+    adapters.remove(0)
+}
